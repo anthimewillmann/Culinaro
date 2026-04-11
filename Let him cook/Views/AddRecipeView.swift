7401c363209ac4deb8bf55b778bebea7cfc0b113
anthimewillmann <anthime.willmann@gmail.com>
2026-04-11 03:52:01 +0200

    Renamed to Culinaro

--- Let him cook/Views/AddRecipeView.swift
+++
@@ -1,7 +0,0 @@
-//
-//  AddRecipeView.swift
-//  Let him cook
-//
-//  Created by Anthime Willmann on 17.12.25.
-//
-
--- Let him cook/Views/RecipesView.swift
+++
@@ -1,7 +0,0 @@
-//
-//  RecipesView.swift
-//  Let him cook
-//
-//  Created by Anthime Willmann on 17.12.25.
-//
-
